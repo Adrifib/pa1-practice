adc3ebf641e1310b5b9ae93771cf2578ec8439a0
Guillem Arnau Vallejos Clavell <guillem.avc@gmail.com>
2024-12-27 13:21:09 +0100

    Inicialización del tablero y lógica de colocación de fichas
    - Se ha implementado la inicialización del tablero y la lista de fichas pendientes (unplayed).
    -Se ha añadido la lógica para la colocación de fichas durante la primera fase del juego, alternando correctamente entre jugadores.

--- abs_board_h.py
+++ abs_board_h.py
@@ -23,9 +23,23 @@ def set_board_up(stones_per_player = 4):
 
     # init board and game data here
 
+    board = [[NO_PLAYER for _ in range(BSIZ)] for _ in range(BSIZ)] # crear el tablero vacío
+
+    unplayed = [
+    Stone(-1, -1, PLAYER_COLOR[player % 2])
+    for player in range(2 * stones_per_player)
+]
+    # crear las piedras de los jugadores que aún no han sido jugadas de manera intercalada (ficha en la posición 0 es azul y en la posición 1 es roja y así sucesivamente)
+
+    played = [] # lista de piedras que ya han sido jugadas
+
+    curr_player = 0 # jugador actual
+
+
     def stones():
         "return iterable with the stones already played"
-        pass
+        return iter(played)
+
 
     def select_st(i, j):
         '''
@@ -35,6 +49,7 @@ def set_board_up(stones_per_player = 4):
         Report success by returning a boolean;
         '''
         pass
+
 
     def end():
         'Test whether there are 3 aligned stones'
@@ -49,7 +64,22 @@ def set_board_up(stones_per_player = 4):
         already selected, current player, and boolean indicating
         the end of the game.
         '''
-        pass
+        nonlocal curr_player # para poder modificar la variable curr_player
+         
+        if 0 <= i < BSIZ and 0 <= j < BSIZ: #comprobar si la piedra esta dentro del tablero
+            if board[i][j] == NO_PLAYER and unplayed: # comprobar si la piedra no ha sido jugada y si hay piedras sin jugar
+                
+                stone = unplayed.pop(0) # sacar la piedra de la lista de piedras sin jugar
+                stone = stone._replace(x=i, y=j)  # Actualizar posición
+                played.append(stone)  # Añadir a piedras en juego
+
+                board[i][j] = curr_player # actualizar el tablero
+
+                curr_player = 1 - curr_player # cambiar de jugador
+            
+            return True, curr_player, False # Devolver el estado: piedra siempre seleccionada, jugador actual, fin del juego (false)
+        return True, curr_player, False 
+
 
     def draw_txt(end = False):
         'Use ASCII characters to draw the board.'
